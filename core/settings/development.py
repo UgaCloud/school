a38fed0c4915cafab28e3fea1b3cc5939c1f6042
watumwa <watumwaizaac32@gmail.com>
2025-04-07 16:31:55 +0300

    changed database to postgres

--- core/settings/development.py
+++ core/settings/development.py
@@ -12,8 +12,8 @@ DATABASES = {
         'NAME': 'schooldb',
         'USER': 'postgres',
         'PASSWORD': 'root',
-        'HOST': '162.254.35.90:8080',  
-        'PORT': '5050',
+        'HOST': '162.254.35.90',  
+        'PORT': '5432',
     }
 }
 
