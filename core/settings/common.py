816dc259a7dc07e76aaf1c0f6a025ff9b37c8a7e
Abdulhakim <walusimbi96@gmail.com>
2025-05-08 07:19:14 +0300

    Modified Database settings
    Separated DB Settings for Development and Production

--- core/settings/common.py
+++ core/settings/common.py
@@ -87,38 +87,38 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.postgresql',
+# #         'NAME': 'schooldb',
+# #         'USER': 'schooluser',
+# #         'PASSWORD': 'root@admin',
+# #         'HOST': 'localhost',
+# #         'PORT': '5432',
+# #     }
+# # }
+
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'schooldb',
 #         'USER': 'schooluser',
-#         'PASSWORD': 'root@admin',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  
+#         'PORT': '3306',
 #     }
 # }
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'schooldb',
-        'USER': 'schooluser',
-        'PASSWORD': '',
-        'HOST': 'localhost',  
-        'PORT': '3306',
-    }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': str(BASE_DIR / 'db.sqlite3'),
-#     }
-# }
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.sqlite3',
+# #         'NAME': str(BASE_DIR / 'db.sqlite3'),
+# #     }
+# # }
 
 
 # Password validation
